844d27cea5e13e58675bab439acac924e5ca206c
RaximboyIbroximov <raximboy4200@gmail.com>
2023-12-21 10:58:19 +0500

    1-version

---
+++ main.py
@@ -0,0 +1,27 @@
+import psycopg2
+
+
+db = psycopg2.connect(
+    database = 'Payshanba',
+    password='1996',
+    user='postgres',
+    host='localhost'
+)
+
+cursor = db.cursor()
+
+cursor.execute('''CREATE TABLE IF NOT EXISTS users(
+   user_id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+   name VARCHAR(40),
+   lastname VARCHAR(40),
+   contact VARCHAR(15) UNIQUE
+)''')
+
+cursor.execute('''INSERT INTO users(name, lastname, contact)
+VALUES (%s, %s, %s)
+ON CONFLICT DO NOTHING''',('Odilbek', 'Murodaov', '12112133132'))
+
+
+
+db.commit()
+db.close()
