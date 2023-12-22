f63362e92d20df1f5fbbc5db88890e562ef15b32
Raximboy Ibrohimov <122911999+Raximboy7@users.noreply.github.com>
2023-12-23 00:21:42 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,27 +1,84 @@
+# import psycopg2
+
+
+# db = psycopg2.connect(
+#     database = 'You basename',
+#     password='You password',
+#     user='postgres',
+#     host='localhost'
+# )
+
+# cursor = db.cursor()
+
+# cursor.execute('''CREATE TABLE IF NOT EXISTS users(
+#    user_id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+#    name VARCHAR(40),
+#    lastname VARCHAR(40),
+#    contact VARCHAR(15) UNIQUE
+# )''')
+
+# cursor.execute('''INSERT INTO users(name, lastname, contact)
+# VALUES (%s, %s, %s)
+# ON CONFLICT DO NOTHING''',('Odilbek', 'Murodaov', '12112133132'))
+
+
+
+# db.commit()
+# db.close()
+
+
 import psycopg2
 
+class DataBase:
+    def __init__(self):
+        self.database = psycopg2.connect(
+            database='Payshanba',
+            password='1996',
+            user='postgres',
+            host='localhost'
 
-db = psycopg2.connect(
-    database = 'You basename',
-    password='You password',
-    user='postgres',
-    host='localhost'
-)
+        )
 
-cursor = db.cursor()
+    def menager(self, sql, *args, commit:bool=False,
+                                fetchone:bool=False,
+                                fetchall:bool=False):
+        with self.database as db:
+            with db.cursor() as cursor:
+                cursor.execute(sql, args)
+                if commit:
+                    return db.commit()
+                elif fetchone:
+                    return cursor.fetchone()
+                elif fetchall:
+                    return cursor.fetchall()
 
-cursor.execute('''CREATE TABLE IF NOT EXISTS users(
-   user_id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
-   name VARCHAR(40),
-   lastname VARCHAR(40),
-   contact VARCHAR(15) UNIQUE
-)''')
-
-cursor.execute('''INSERT INTO users(name, lastname, contact)
-VALUES (%s, %s, %s)
-ON CONFLICT DO NOTHING''',('Odilbek', 'Murodaov', '12112133132'))
+    def create_table_categories(self):
+        sql = '''CREATE TABLE IF NOT EXISTS categories(
+            category_id SERIAL PRIMARY KEY,
+            category_name VARCHAR(60) UNIQUE
+        )'''
+        self.menager(sql, commit=True)
 
 
+    def insert_category(self, category_name):
+        sql = '''INSERT INTO categories(category_name)
+        VALUES (%s) ON CONFLICT DO NOTHING'''
+        self.menager(sql, (category_name,), commit=True)
 
-db.commit()
-db.close()
+
+    def get_all_categories(self):
+        sql = '''SELECT * FROM categories'''
+        return self.menager(sql, fetchall=True)
+
+
+db = DataBase()
+# db.create_table_categories()
+# categories = ['kompyuter', 'televizor', 'smartfon']
+# for category in categories:
+#    db.insert_category(category)
+
+
+
+# print(db.get_all_categories())
+
+
